f13a3b2013742a9b6e5d0279e37652a581da7dd3
Elder Patten Ferreira <elderferreirass@gmail.com>
2020-08-02 16:58:00 -0700

    Add update, delete, and subscription

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { useEffect, useReducer } from 'react';
+import { v4 as uuid } from 'uuid';
 import { API } from 'aws-amplify';
-import { List } from 'antd';
+import { List, Input, Button } from 'antd';
 import 'antd/dist/antd.css';
 import { listNotes } from './graphql/queries';
+import {
+  createNote as CreateNote,
+  deleteNote as DeleteNote,
+  updateNote as UpdateNote,
+} from './graphql/mutations';
+import { onCreateNote } from './graphql/subscriptions';
 import './App.css';
+
+const CLIENT_ID = uuid();
 
 const styles = {
   container: { padding: 20 },
@@ -21,6 +30,15 @@ const initialState = {
 
 const reducer = (state, action) => {
   switch (action.type) {
+    case 'ADD_NOTE':
+      return { ...state, notes: [action.note, ...state.notes]};
+    case 'RESET_FORM':
+      return { ...state, form: initialState.form }
+    case 'SET_INPUT':
+      return { ...state, form: {
+        ...state.form,
+          [action.name]: action.value,
+        }};
     case 'SET_NOTES':
       return { ...state, notes: action.notes, loading: false };
     case 'ERROR':
@@ -40,14 +58,79 @@ function App() {
       });
       dispatch({ type: 'SET_NOTES', notes: notesData.data.listNotes.items })
     } catch (err) {
-      console.log(err);
       dispatch({ type: 'ERROR' })
     }
   };
 
+  const createNote = async () => {
+    const { form } = state;
+    if (!form.name || !form.description) {
+      return alert('please enter a name and description');
+    }
+    const note = { ...form, clientId: CLIENT_ID, completed: false, id: uuid() };
+    dispatch({ type: 'RESET_FORM' });
+
+    try {
+      await API.graphql({
+        query: CreateNote,
+        variables: {
+          input: note,
+        }
+      })
+    } catch (err) {
+      dispatch({ type: 'ERROR' })
+    }
+  };
+
+  const deleteNote = async ({ id }) => {
+    const index = state.notes.findIndex(n => n.id === id);
+    const notes = [
+      ...state.notes.slice(0, index),
+      ...state.notes.slice(index + 1)
+    ];
+    dispatch({ type: 'SET_NOTES', notes });
+
+    try {
+      await API.graphql({
+        query: DeleteNote,
+        variables: { input: { id } }
+      })
+    } catch (err) {
+      dispatch({ type: 'ERROR' })
+    }
+  };
+
+  const updateNote = async (note) => {
+    const index = state.notes.findIndex(n => n.id === note.id);
+    const notes = [...state.notes];
+    notes[index].completed = !note.completed;
+    dispatch({ type: 'SET_NOTES', notes });
+    try {
+      await API.graphql({
+        query: UpdateNote,
+        variables: {
+          input: {
+            id: note.id,
+            completed: notes[index].completed,
+          }
+        }
+      })
+    } catch (err) {
+      dispatch({ type: 'ERROR' })
+    }
+  };
+
+  const onChange = (e) => {
+    dispatch({ type: 'SET_INPUT', name: e.target.name, value: e.target.value })
+  };
+
   const renderItem = (item) => {
+    const { completed } = item;
     return (
-      <List.Item style={styles.item}>
+      <List.Item style={styles.item} actions={[
+        <p style={styles.p} onClick={() => deleteNote(item)}>Delete</p>,
+        <p style={styles.p} onClick={() => updateNote(item)}>{completed ? 'completed' : 'mark completed'}</p>
+      ]}>
         <List.Item.Meta
           title={item.name}
           description={item.description}
@@ -58,10 +141,41 @@ function App() {
 
   useEffect(() => {
     fetchNotes();
+    const subscription = API.graphql({
+      query: onCreateNote,
+    }).subscribe({
+      next: noteData => {
+        const note = noteData.value.data.onCreateNote;
+        if (CLIENT_ID === note.clientId) {
+          return dispatch({ type: 'ADD_NOTE', note })
+        }
+      }
+    });
+
+    return () => subscription.unsubscribe();
   }, []);
 
   return (
     <div style={styles.container}>
+
+      <Input
+        onChange={onChange}
+        value={state.form.name}
+        placeholder="Note name"
+        name="name"
+        style={styles.input}
+      />
+      <Input
+        onChange={onChange}
+        value={state.form.description}
+        placeholder="Note description"
+        name="description"
+        style={styles.input}
+      />
+      <Button
+        onClick={createNote}
+        type="primary"
+      >Create Note</Button>
       <List
         loading={state.loading}
         dataSource={state.notes}
